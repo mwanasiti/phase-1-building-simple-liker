64b9e481edaa540b0a4f49f3f4cc79f2efa6d513
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-19 17:10:14 +0300

    simple

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const sinon=require('sinon');
+const helpers=require ('./helpers');
+const chai=require ('chai');
+const spies=require ('chai-spies');
 
+chai.use(spies);
+
+describe("main.js", ()=>){
+  it("contains a hidden modal", ()=>{
+    let modal=document.querySelector(".hidden")
+    expect (modal).not.to.equal(null)
+  })
+}
 
 
 
